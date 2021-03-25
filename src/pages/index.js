82cd6de3a1ef4bb962e4c4f86b9ccfafa6decd95
Dimitri ONGOUA <ongouadimitri5@gmail.com>
2021-03-25 22:44:42 +0100

    link over pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,15 @@
+import { Link } from "gatsby"
 import React from "react"
+import Header from "../components/header"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div style={{ color: `purple` }}>
+      <Link to="/contact/">Contact</Link>
+      <Header headerText="Hello Boss!" />
+      <p>What a world.</p>
+
+      <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </div>
+  )
 }
